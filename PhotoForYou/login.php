31b0fd9faa8b340b9026eb3fb5de9fd8d60ad72d
SMPK10 <65543488+SMPK10@users.noreply.github.com>
2021-05-07 16:34:12 +0200

    Delete login.php

--- PhotoForYou/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Préparation du login à la base de données
-try {
-	$base = new PDO('mysql:host=localhost; dbname=pfy; charset=utf8','root','');
-} catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
-}
-?>
